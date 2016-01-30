5bc29709a1cf5b6bdc99a70d6949c9fb999feb3d
kspi <k.spiridonovas@gmail.com>
2016-01-30 10:52:25 +0200

    helpers: Add shortened weekday names.

--- agenda/helpers.py
+++ agenda/helpers.py
@@ -1,6 +1,7 @@
 from datetime import timedelta, date
 
 monday, tuesday, wednesday, thursday, friday, saturday, sunday = range(7)
+mon, tue, wed, thur, fri, sat, sun = range(7)
 
 def is_nth_weekday(weekday, n, day):
     d = n + 1 if n >= 0 else n
